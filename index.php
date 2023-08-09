903d6044db455f06f297b33739daf65202ad3909
Ishan Shrestha <ishanstha1998@gmail.com>
2023-08-09 18:07:11 +0545

    adding-translators

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
     
 <?php if(have_posts()) { ?>
-    <?php while(have_posts()) { ?>
-        <?php the_post(); ?>
+    <?php while(have_posts()) {?>
+      <?php the_post(); ?>
         <h2>
-            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php the_title() ?></a>
+            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a>
         </h2>
         <div>
             <?php firsttheme_post_meta(); ?>
@@ -14,9 +14,9 @@
         </div>
         <?php firsttheme_read_more_link(); ?>
     <?php } ?>
-    <?php the_posts_pagination() ?>
+    <?php the_posts_pagination(); ?>
 <?php } else { ?>
-    <p>Sorry, no posts matched your criteria.</p>
+    <p><?php _e('Sorry, no posts matched your criteria.', 'firsttheme'); ?></p>
 <?php } ?>
 
 <?php get_footer(); ?>
--- lib/helper.php
+++ lib/helper.php
@@ -1,15 +1,35 @@
 <?php
 function firsttheme_post_meta(){
-    echo 'Posted on ';
-    echo '<a href="' .esc_url(get_permalink()). '">';
-    echo '<time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date()) . '</time>';
-    echo '</a>';
-    echo' By <a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . esc_html(get_the_author()) .
-    '</a>';
+    /* translators: %s: Post Date */
+    printf(
+        esc_html__('Posted on %s','firsttheme'),
+        '<a href="' .esc_url(get_permalink()). '"><time datetime="' . esc_attr(get_the_date('c')) .
+        '">' . esc_html(get_the_date()) . '</time></a>'
+    );
+    /* translators: %s: Post Author */
+    printf(
+        esc_html__(' By %s', 'firsttheme'),
+        '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . esc_html(get_the_author()) .
+        '</a>'
+    );
+   
 }
 function firsttheme_read_more_link(){
-    echo '<a href="' . esc_url(get_permalink()) . '" title="' . esc_attr(the_title_attribute(['echo'=>false])) . '">';
-    echo 'Read More <span class="u-screen-reader-text">About '  . get_the_title() . '</span>';
+    echo '<a href="' . esc_url(get_permalink()) . '" title="' . esc_attr(the_title_attribute(['echo'=>false])) .
+     '">';
+    /* translators: %s: Post Title */
+    printf(
+        wp_kses(
+            __('Read More <span class="u-screen-reader-text">About %s </span>','firsttheme'),
+            [
+                'span'=> [
+                    'class' => []
+                ]
+            ]
+        ),
+        get_the_title()
+
+    );
     echo '</a>';
 }
 
